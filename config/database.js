e9deabb122e1517bf8876ac50722ab55ee692b6f
edengebrezgi <edengebrezgi@gmail.com>
2021-04-03 23:38:14 -0400

    add file

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://edengebrezgi:aaliyah@cluster0.fakzp.mongodb.net/roulette?retryWrites=true&w=majority',
+  'dbName': 'roulette'
+};
